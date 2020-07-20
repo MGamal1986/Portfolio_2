c9177ab75a16251769788ea6af506247088e68f6
MGamal1986 <elsayedmg@gmail.com>
2020-07-20 14:04:45 +0200

    start in about section

--- src/js/components/sideNav.js
+++ src/js/components/sideNav.js
@@ -1,11 +1,15 @@
 // select nav elements
 const nav  = document.querySelector('.btn-sections');
 const mainElement = document.querySelector('.btn-sections .main');
+
 // import sidebar component 
 import SideBar from './sideBar';
 
 export default function SideNav(){
+    // create instance of SideBar class
     let sideNav = new SideBar(mainElement,nav,'small-screen','up');
+    // call show function
     sideNav.show();
+    // call hide function
     sideNav.hide();
 }
